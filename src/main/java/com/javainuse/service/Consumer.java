6e026855c6d7b64aeda12ee7c6d41ae3d5029b29
jomcdona <jomcdona@redhat.com>
2020-09-09 15:31:05 +0000

    updated consumer

--- src/main/java/com/javainuse/service/Consumer.java
+++ src/main/java/com/javainuse/service/Consumer.java
@@ -1,3 +1,5 @@
+package com.javainuse.service;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
